ee2335b6e22bd465a15318d4fbb03013f87889d6
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-10-18 19:05:43 +0900

    햄토리 이동 구현

--- Hamtori_Game.py
+++ Hamtori_Game.py
@@ -1,82 +1,67 @@
 from pico2d import *
+
 open_canvas()
+running = True
 
-running=True
+y = 600 // 2
+x = 800 // 2
+frame = 0
+dirx = 0
+diry = 0
+ham_image_judge = 1  # 초기 이미지 방향 설정
 
-
-y=600//2
-x=800//2
-frame=0
-dirx=0
-diry=0
-
-
-###################################클래스들##############################
+################################### 클래스들 ##############################
 class Hamtori_Image:
-    ham_up=('Hams_Walking_Up.png')
-    ham_down=('Hams_Walking_Down.png')
-    ham_left_rigth=('Hams_walking_left_right.png')
-
-
+    ham_up = 'Hams_Walking_Up.png'
+    ham_down = 'Hams_Walking_Down.png'
+    ham_left_right = 'Hams_walking_left_right.png'
 
 class Hamtori:
-    def __init__(self, ham_image_judge=1):
-        self.x, self.y = 20, 20
+    def __init__(self):
+        self.x, self.y = 20, 20  # 화면 왼아래에서 시작
         self.frame = 0
-        if ham_image_judge == 1:
-            self.ham_image = load_image(Hamtori_Image.ham_up)
-        elif ham_image_judge == 2:
-            self.ham_image = load_image(Hamtori_Image.ham_down)
-        elif ham_image_judge == 3:
-            self.ham_image = load_image(Hamtori_Image.ham_left_right)    
+        self.ham_image_judge = 1  # 초기 이미지 설정
+        self.ham_image = load_image(Hamtori_Image.ham_left_right)  # 기본 이미지를 오른쪽 이동 이미지로 설정
+
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.frame = (self.frame + 1) % 4  # 애니메이션 프레임 업데이트
 
     def draw(self):
-        self.ham_image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-
-    def moving_left(ham_image_judge):
-        ham_image_judge=3
+        # 방향에 따라 이미지를 선택
+        if self.ham_image_judge == 1:  # 위로 이동
+            self.ham_image = load_image(Hamtori_Image.ham_up)
+            self.ham_image.clip_draw(self.frame * 37, 0, 37, 40, self.x, self.y)
+        elif self.ham_image_judge == 2:  # 아래로 이동
+            self.ham_image = load_image(Hamtori_Image.ham_down)
+            self.ham_image.clip_draw(self.frame * 40, 0, 40, 40, self.x, self.y)
+        elif self.ham_image_judge == 3:  # 좌 이동
+            self.ham_image = load_image(Hamtori_Image.ham_left_right)
+            self.ham_image.clip_draw(self.frame * 37-3, 0, 37, 40, self.x, self.y)
+        elif self.ham_image_judge == 4:  # 우 이동
+            self.ham_image = load_image(Hamtori_Image.ham_left_right)
+            self.ham_image.clip_draw(144+self.frame * 37, 0, 37, 40, self.x, self.y)
         
-        pass
-    def moving_right():
-        pass
-    def moving_up():
-        pass
-    def moving_down():
-        pass
-    def idle():
-        pass
-    def running_left():
-        pass
-    def running_right():
-        pass
-    def running_up():
-        pass
-    def running_down():
-        pass
+       
 
-class enemy_1:
-    pass
-class enemy_2:
-    pass
-class Background:
-    pass
-class wall:
-    wall_image=None
-    if wall_image==None:
-        wall_image=('wall.png')
-    pass
-class obstacle:
-    pass
-class Events:
-    global running
-    def escape_event():
-        running=False
-####################################함수들##############################
+        
 
+    def move_left(self):
+        self.ham_image_judge = 3  # 왼쪽으로 이동하는 이미지로 설정
+        self.x -= 5  # 왼쪽으로 이동
 
+    def move_right(self):
+        self.ham_image_judge = 4  # 오른쪽으로 이동하는 이미지로 설정
+        self.x += 5  # 오른쪽으로 이동
 
+    def move_up(self):
+        self.ham_image_judge = 1  # 위로 이동하는 이미지로 설정
+        self.y += 5  # 위로 이동
+
+    def move_down(self):
+        self.ham_image_judge = 2  # 아래로 이동하는 이미지로 설정
+        self.y -= 5  # 아래로 이동
+
+#################################### 함수들 ##############################
 def handle_events():
     global dirx, diry, running
     events = get_events()
@@ -100,33 +85,35 @@ def handle_events():
             elif event.key in (SDLK_UP, SDLK_DOWN):
                 diry = 0
 
-
 def update_world():
-    pass
-
+    global dirx, diry
+    if dirx == -1:
+        hamtori.move_left()
+    elif dirx == 1:
+        hamtori.move_right()
+    if diry == 1:
+        hamtori.move_up()
+    elif diry == -1:
+        hamtori.move_down()
+    
+    hamtori.update()  # 애니메이션 프레임 업데이트
 
 def render_world():
-    pass
-def reset_world(): #초기화 함수
-    pass
+    clear_canvas()  # 화면을 지움
+    hamtori.draw()  # 햄토리 그리기
+    update_canvas()  # 화면 업데이트
 
-def stage_1():  #1스테이지
-    pass
-def stage_2():  #2스테이지
-    pass
-def stage_3():  #3스테이지
-    pass
-def stage_4():  #4스테이지
-    pass
+def reset_world():
+    global hamtori
+    hamtori = Hamtori()  # 햄토리 객체 생성
 
-open_canvas()
-
-# initialization code 객체를 초기에 창조함
+################################### 게임 실행 ###################################
 reset_world()
+
 while running:
-    handle_events() #입력 이벤트 처리
-    update_world()
-    render_world()
-    delay(0.05) 
+    handle_events()  # 입력 이벤트 처리
+    update_world()  # 캐릭터 이동 및 상태 업데이트
+    render_world()  # 화면에 그리기
+    delay(0.05)
 
 close_canvas()
